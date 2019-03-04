705b44eaf99e5f48bcbb36d87fc73160087e97c0
einomies26 <einokamarainen@hotmail.com>
2019-03-04 23:22:31 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Contact from './Contact';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/contact">Contact</Link>{' '}
+            <Link to="/links">Links</Link>{' '}
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/links" render={() => <h1>Links</h1>} />
+              <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
